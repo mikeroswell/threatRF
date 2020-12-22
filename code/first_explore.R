3d9b3e46e4a364776b359c6b8a1afeec8e459174
mikeroswell <mike.roswell@gmail.com>
2020-12-22 13:37:34 -0500

    I think i'm going to abondon this repo and use MD_Bee_data

--- code/first_explore.R
+++ code/first_explore.R
@@ -1,8 +1,5 @@
 ######## HOW MANY BEES ARE ON IUCN (TOTAL, NOT STRENONITIDAE)
 
-library(tidyverse)
-library(rgbif)
-
 assmnt <- read.csv("data/IUCNassessments.csv")
 ids <- read.csv("data/IUCNtaxonomy.csv")
 
@@ -19,25 +16,3 @@ assmnt %>% group_by(realm) %>% summarize(n())
 with_status<-data.table::fread("data/ObsMDBeesConservStat.csv")
 head(with_status)
 View(with_status %>% group_by(name, Conservation.Status) %>% summarize(n()) %>% arrange(desc(Conservation.Status)))
-
-
-################# get gbif records
-
-get_gbif <- function(taxon
-                   , min.lat = 37.8889
-                   , max.lat = 39.722
-                   , min.lon = -79.4861
-                   , max.lon = -74.8581
-                   , min.year = 1981
-                   , max.year = 2020
-                   ){
-  occ_data(scientificName = taxon
-           , country ="US"
-           , hasCoordinate = T
-           , year=paste0(min.year, ",", max.year)
-           , decimalLatitude = paste0(min.lat, ",", max.lat)
-           , decimalLongitude = paste0(min.lon, ",", max.lon)
-           , limit = 5e5)
-}
-test_bif<-get_gbif("Andrena carlini")
-test_bif
