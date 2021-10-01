bc1059268442cc121dc19192dbcdab49eb008f89
mikeroswell <mike.roswell@gmail.com>
2021-09-30 22:03:34 -0400

    seems like minlat and minlong type stuff is cheating, since those are species names.

--- code/fit_RF.R
+++ code/fit_RF.R
@@ -2,62 +2,93 @@
 
 library(tidyverse)
 library(randomForest)
+`%ni%` <- Negate(`%in%`) #convenience, this should be part of base R!
 
 load(file="data/fromR/to_predict.RDA")
+unique(indi$roundedSRank)
 
+tofit<-indi %>% dplyr::mutate(lat = sf::st_coordinates(.)[,1],
+                              lon = sf::st_coordinates(.)[,2]) %>% 
+  group_by(genus, species) %>%
+  mutate(maxlat = max(lat, na.rm =T)
+         , minlat = min(lat, na.rm = T)
+         , maxlon = max(lon, na.rm = T)
+         , minlon = min(lon, na.rm = T)
+         , simple_status = factor(if_else(roundedSRank %in% c("S4","S5"), "secure"
+                                   , if_else(roundedSRank %in% c("S1", "S2", "S3", "SH"), "threatened", "NONE")))) %>% 
+  sf::st_drop_geometry()
 
-tofit<-indi %>% group_by(genus, species) %>%
-  mutate(maxlat = max(latitude, na.rm =T)
-         , minlat = min(latitude, na.rm = T)
-         , maxlon = max(longitude, na.rm = T)
-         , minlon = min(longitude, na.rm = T)
-         , simple_status = as.factor(simple_status))
 
 resps<-unique(tofit$simple_status)
 
-predictors<-  c("LU_CODE"
-, "bioclim1"
-, "bioclim2"
-, "bioclim3"
-, "bioclim4"
-, "bioclim5"
-, "bioclim6"
-, "bioclim7"
-, "bioclim8"
-, "maxlat"
-, "minlat"
-, "maxlon"
-, "minlon"
-)
-tofit_complete<-tofit %>%  drop_na(eval(predictors))
 
-first_RF_training <- randomForest(simple_status ~
-                           LU_CODE
-                         + bioclim1
-                         + bioclim2
-                         + bioclim3
-                         + bioclim4
-                         + bioclim5
-                         + bioclim6
-                         + bioclim7
-                         + bioclim8
-                         + maxlat
-                         + minlat
-                         + maxlon
-                         + minlon
-                         , data = tofit_complete %>% filter(simple_status %in% c("threat", "secure")) %>% droplevels()
+tofit_trimmed<-tofit %>% 
+  ungroup %>% 
+  select(-c(  ))
+
+
+names(tofit)<-gsub("\\.", "A", names(tofit))
+
+predictors<-  names(tofit)[names(tofit) %ni% c( "roundedSRank", "roundedNRank", "roundedGRank", "genus", "species", "lat", "lon", "simple_status", "geometry", names(tofit)[grepl("OBJECTID*", names(tofit))], names(tofit)[grepl("Descriptio*", names(tofit))] )]
+
+
+
+
+tofit_complete<-tofit %>% drop_na(eval(predictors))
+# tofit_complete %>% filter(simple_status %in% c("threatened", "secure")) %>% droplevels() %>% group_by(simple_status) %>% summarize(n())
+
+
+first_RF_training <- randomForest(as.formula(paste0("simple_status ~ ", paste(predictors, collapse= "+")))
+                           
+                         , data = tofit_complete %>% filter(simple_status %in% c("threatened", "secure")) %>% droplevels()
                          # , ytest = c("threat", "secure")
                          , importance=TRUE
                          , na.action = na.exclude
+                         , type = "classification"
 )
+
+
+first_RF_training
+plot(first_RF_training)
 
 pdf("figures/toy_model_variable_importance.pdf")
 varImpPlot(first_RF_training)
 dev.off()
 
-first_RF_preds<- predict(first_RF_training, data = tofit_complete %>% filter(simple_status =="unranked"))
+first_RF_preds<- predict(first_RF_training, newdata = tofit_complete %>% filter(simple_status =="NONE"))
 summary(first_RF_preds)
 str(first_RF_preds)
-with_preds<-bind_cols(tofit_complete %>% filter(simple_status =="unranked"), first_RF_preds)
-prediction_rates = with_preds %>% group_by(genus, species) %>% summarize(records = n(), threatrate = sum(first_RF_preds =="threat")/n())
+with_preds<-bind_cols(tofit_complete %>% filter(simple_status =="NONE"), first_RF_preds)
+
+
+# get rid of variables that work as species names
+noname<-predictors[predictors %ni% c("minlat", "minlon", "maxlat", "maxlon")]
+noname_RF_training <- randomForest(as.formula(paste0("simple_status ~ ", paste(noname, collapse= "+")))
+                                 
+                                 , data = tofit_complete %>% filter(simple_status %in% c("threatened", "secure")) %>% droplevels()
+                                 # , ytest = c("threat", "secure")
+                                 , importance=TRUE
+                                 , na.action = na.exclude
+                                 , type = "classification"
+)
+
+pdf("figures/variable_importance.pdf")
+varImpPlot(noname_RF_training)
+dev.off()
+
+noname_RF_training
+plot(noname_RF_training)
+
+noname_RF_preds<- predict(noname_RF_training, newdata = tofit_complete %>% filter(simple_status =="NONE"))
+summary(noname_RF_preds)
+str(noname_RF_preds)
+with_preds<-bind_cols(tofit_complete %>% filter(simple_status =="NONE"), noname_RF_preds)
+
+
+prediction_rates = with_preds %>% group_by(genus, species) %>% summarize(records = n(), threatrate = sum(noname_RF_preds =="threatened")/n())
+prediction_rates %>% ggplot(aes(records, threatrate))+
+  scale_x_log10()+
+  scale_y_log10()+
+  geom_jitter()+
+  theme_classic()
 
