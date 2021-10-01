4004caba26b6d0f2ba75570942cf64f4f5fa3b0b
mikeroswell <mike.roswell@gmail.com>
2021-10-01 00:34:37 -0400

    making maps

--- code/fit_RF.R
+++ code/fit_RF.R
@@ -19,7 +19,9 @@ tofit<-indi %>% dplyr::mutate(lat = sf::st_coordinates(.)[,1],
   sf::st_drop_geometry()
 
 
-resps<-unique(tofit$simple_status)
+# tofit %>% mutate(gs = paste(genus, species, sep = "_")) %>% group_by(simple_status) %>% summarize(genera = n_distinct(genus), spp = n_distinct(gs))
+
+
 
 
 tofit_trimmed<-tofit %>% 
@@ -50,7 +52,7 @@ first_RF_training <- randomForest(as.formula(paste0("simple_status ~ ", paste(pr
 
 first_RF_training
 plot(first_RF_training)
-
+data.frame(first_RF_training$importance) %>% arrange(desc(MeanDecreaseGini))
 pdf("figures/toy_model_variable_importance.pdf")
 varImpPlot(first_RF_training)
 dev.off()
@@ -72,6 +74,35 @@ noname_RF_training <- randomForest(as.formula(paste0("simple_status ~ ", paste(n
                                  , type = "classification"
 )
 
+
+# use species names, not min/max lat/lon
+withname<- names(tofit)[names(tofit) %ni% c( "roundedSRank", "roundedNRank", "roundedGRank", "genus", "lat", "lon", "simple_status", "geometry", names(tofit)[grepl("OBJECTID*", names(tofit))], names(tofit)[grepl("Descriptio*", names(tofit))], "minlat", "minlon", "maxlat", "maxlon")]
+withname_RF_training <- randomForest(as.formula(paste0("simple_status ~ ", paste(withname, collapse= "+")))
+                                   
+                                   , data = tofit_complete %>% filter(simple_status %in% c("threatened", "secure")) %>% droplevels()
+                                   # , ytest = c("threat", "secure")
+                                   , importance=TRUE
+                                   , na.action = na.exclude
+                                   , type = "classification"
+)
+varImpPlot(withname_RF_training)
+withname_RF_training
+
+
+names_and<- names(tofit)[names(tofit) %ni% c( "roundedSRank", "roundedNRank", "roundedGRank", "genus", "lat", "lon", "simple_status", "geometry", names(tofit)[grepl("OBJECTID*", names(tofit))], names(tofit)[grepl("Descriptio*", names(tofit))])]
+
+namesand_RF_training <- randomForest(as.formula(paste0("simple_status ~ ", paste(names_and, collapse= "+")))
+                                     
+                                     , data = tofit_complete %>% filter(simple_status %in% c("threatened", "secure")) %>% droplevels()
+                                     # , ytest = c("threat", "secure")
+                                     , importance=TRUE
+                                     , na.action = na.exclude
+                                     , type = "classification"
+)
+
+varImpPlot(namesand_RF_training)
+namesand_RF_training
+
 pdf("figures/variable_importance.pdf")
 varImpPlot(noname_RF_training)
 dev.off()
@@ -81,11 +112,13 @@ dev.off()
 
 phigh<-predict(first_RF_training, newdata = tofit_complete)
 plow<-predict(noname_RF_training, newdata = tofit_complete)
+pname<-predict(withname_RF_training, newdata = tofit_complete)
 
 all_pred<-tofit_complete %>% bind_cols(phigh_stat = phigh, plow_stat = plow)
 
-get_a_picture <- function(prediction){
-  all_pred %>% 
+get_a_picture <- function(x, prediction){
+  print(prediction)
+  x %>% 
     group_by(genus, species, simple_status) %>% 
     summarize(agree = sum(as.character(get(prediction)) == as.character(simple_status))/n()
               , frequency_threatened = sum(get(prediction) =="threatened")/n()
@@ -97,5 +130,23 @@ picture<-function(df){df %>% ggplot(aes(simple_status, frequency_threatened, col
   theme_classic() +
   scale_color_viridis_c()}
 
+pdf("figures/threat_prediction_frequency.pdf")
+picture(get_a_picture("phigh_stat"))
+dev.off()
 
+# make some maps
 
+resf<-sf::st_as_sf(all_pred
+                   , coords = c("lat", "lon")
+                   , crs = "+proj=aea +lat_0=23 +lon_0=-96 +lat_1=29.5 +lat_2=45.5 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs")
+
+fancy_sf<-resf %>%  group_by(genus, species, simple_status) %>%   
+  mutate(agree = sum(as.character(phigh_stat) == as.character(simple_status))/n()
+            , frequency_threatened = sum(phigh_stat =="threatened")/n()
+            , n_obs= n()
+         , best_guess = if_else(simple_status =="NONE", phigh_stat, simple_status))
+
+fancy_sf %>% ggplot(aes(color = best_guess))+
+  geom_sf(size = 1, alpha =0.8) +
+  theme_classic() +
+  scale_color_viridis_d()
