b0e6b6bd4a552e297f1c21614153e2df99a93cab
mikeroswell <mike.roswell@gmail.com>
2022-06-15 12:02:03 -0400

    This might be right columns, double check further

--- code/crunch_GIS.R
+++ code/crunch_GIS.R
@@ -31,13 +31,13 @@ withstats2 <- bind_rows(pstats
 good_coords<- withstats2 %>%
   filter(decimalLatitude<44 & decimalLatitude> 34 &decimalLongitude>-82 & decimalLongitude < -73) %>% 
   # grab only species not listed as introduced
-  filter(!exotic...175| !exotic...167) %>% 
+  filter(!exotic...175| !exotic...173) %>% 
   mutate(UID = rownames(.)) # some errors, check workflow that they weren't introduced here.
 
 exotic_records <- withstats2 %>%
   filter(decimalLatitude<44 & decimalLatitude> 34 &decimalLongitude>-82 & decimalLongitude < -73) %>% 
   # grab only species listed as introduced
-  filter(exotic...175| exotic...167) %>% 
+  filter(exotic...175| exotic...173) %>% 
   mutate(UID = rownames(.)) # some errors, check workflow that they weren't introduced here.
 
 write.csv(exotic_records, file = "data/fromR/lfs/exotic_records_removed.csv")
