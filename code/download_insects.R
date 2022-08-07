9521e7d39aa1fb6c49d9309425ccac7681b2841e
mikeroswell <mike.roswell@gmail.com>
2022-08-07 11:05:45 -0400

    all files from mbp 7 Aug

--- code/download_insects.R
+++ code/download_insects.R
@@ -1,84 +1,67 @@
-#get bug occurrences and statuses
-# update natserv from time to tim
-install.packages("/System/Volumes/Data/Rstudio_Git/natserv_1.0.0.92.tar.gz", repos = NULL)
-library(natserv) #this is how we get naturserve status
-library(rgbif) #how we get gbif observations
-library(tidyverse)
-library(patchwork)
-
-# when downloading data from gbif, will need to combine different record types into DF
-bind.gbif<-function(gbif){bind_rows(gbif[[2]][[3]], gbif[[3]][[3]])}
-
-
-
-# See if we can get the taxonKey for Lepidoptera and then for bees
-
-#don't know how to do this smart but using the webtool drop down I found the taxon keys for the bee families, then make a semicolon-separated string to search.
-# halic<-7908
-# apida<-4334
-# andre<-7901
-# megac<-7911
-# colle<-7905
-# melli<-4345
+# #get bug occurrences and statuses
+# # update natserv from time to tim
+# install.packages("/System/Volumes/Data/Rstudio_Git/natserv_1.0.0.92.tar.gz", repos = NULL)
+# library(natserv) #this is how we get naturserve status
+# library(rgbif) #how we get gbif observations
+# library(tidyverse)
+# library(patchwork)
 # 
-# keyall<-paste(halic, apida, andre, megac, colle, melli, sep=";")
+# # when downloading data from gbif, will need to combine different record types into DF
+# bind.gbif<-function(gbif){bind_rows(gbif[[2]][[3]], gbif[[3]][[3]])}
 # 
-# MD_hym <- occ_search(taxonKey = keyall
-#                      , stateProvince = "Maryland", year="1989, 2019"
-#                      , basisOfRecord = c("OBSERVATION", "HUMAN_OBSERVATION", "PRESERVED_SPECIMEN")
-#                      , hasCoordinate = T
-#                      , limit = 1e5)
 # 
-# hym_flat<-bind.gbif(MD_hym)
-
-MD_lep_old<- occ_search(taxonKey = 797
-                    , stateProvince = "Maryland", year="1989, 2005"
-                    , basisOfRecord = c("OBSERVATION", "HUMAN_OBSERVATION", "PRESERVED_SPECIMEN")
-                    , hasCoordinate = T
-                    , limit = 1e5)
-lep_flat_old<-bind.gbif(MD_lep_old)
-
-MD_lep_new<- occ_search(taxonKey = 797
-                        , stateProvince = "Maryland", year="2006, 2021"
-                        , basisOfRecord = c("OBSERVATION", "HUMAN_OBSERVATION", "PRESERVED_SPECIMEN")
-                        , hasCoordinate = T
-                        , limit = 1e5)
-lep_flat_new<-bind.gbif(MD_lep_new)
-
-lep_flat <-bind_rows(lep_flat_new, lep_flat_old)
-
-leps_gs<-lep_flat %>% separate(acceptedScientificName, sep =" "
-                      , into =c("genus", "species")) %>% # separate just drops stuff after the first two!
-  mutate(gs = paste(genus, species, sep = "_")
-         , withspace = paste(genus, species, sep = " ")) #convenient to keep track of binomials in several forms?
-leps_gs<-leps_gs %>% filter(!grepl("\\.", .$species))
-# write gbif data to file (make these steps modular since they take a long time)
-write.csv(leps_gs, "data/fromR/lfs/leps_direct_from_gbif.csv", row.names = F)
+# 
+# # See if we can get the taxonKey for Lepidoptera and then for bees
+# 
+# #don't know how to do this smart but using the webtool drop down I found the taxon keys for the bee families, then make a semicolon-separated string to search.
+# # halic<-7908
+# # apida<-4334
+# # andre<-7901
+# # megac<-7911
+# # colle<-7905
+# # melli<-4345
+# # 
+# # keyall<-paste(halic, apida, andre, megac, colle, melli, sep=";")
+# # 
+# # MD_hym <- occ_search(taxonKey = keyall
+# #                      , stateProvince = "Maryland", year="1989, 2019"
+# #                      , basisOfRecord = c("OBSERVATION", "HUMAN_OBSERVATION", "PRESERVED_SPECIMEN")
+# #                      , hasCoordinate = T
+# #                      , limit = 1e5)
+# # 
+# # hym_flat<-bind.gbif(MD_hym)
+# 
+# MD_lep_old<- occ_search(taxonKey = 797
+#                     , stateProvince = "Maryland", year="1989, 2005"
+#                     , basisOfRecord = c("OBSERVATION", "HUMAN_OBSERVATION", "PRESERVED_SPECIMEN")
+#                     , hasCoordinate = T
+#                     , limit = 1e5)
+# lep_flat_old<-bind.gbif(MD_lep_old)
+# 
+# MD_lep_new<- occ_search(taxonKey = 797
+#                         , stateProvince = "Maryland", year="2006, 2021"
+#                         , basisOfRecord = c("OBSERVATION", "HUMAN_OBSERVATION", "PRESERVED_SPECIMEN")
+#                         , hasCoordinate = T
+#                         , limit = 1e5)
+# lep_flat_new<-bind.gbif(MD_lep_new)
+# 
+# lep_flat <-bind_rows(lep_flat_new, lep_flat_old)
+# 
+# leps_gs<-lep_flat %>% separate(acceptedScientificName, sep =" "
+#                       , into =c("genus", "species")) %>% # separate just drops stuff after the first two!
+#   mutate(gs = paste(genus, species, sep = "_")
+#          , withspace = paste(genus, species, sep = " ")) #convenient to keep track of binomials in several forms?
+# leps_gs<-leps_gs %>% filter(!grepl("\\.", .$species))
+# # write gbif data to file (make these steps modular since they take a long time)
+# write.csv(leps_gs, "data/fromR/lfs/leps_direct_from_gbif.csv", row.names = F)
 
 # lep_flat
 # check number of spp in this dataset (or at least species names, should check)
-leps_of_MD <- unique(leps_gs$withspace) 
-length(leps_of_MD) #1786, an increase (maybe inat records getting research-graded?)
+# leps_of_MD <- unique(leps_gs$withspace) 
+# length(leps_of_MD) #1786, an increase (maybe inat records getting research-graded?)
 ###################################################
-leps_gs <- read.csv("data/fromR/lfs/leps_direct_from_gbif.csv")
+# leps_gs <- read.csv("data/fromR/lfs/leps_direct_from_gbif.csv")
 #next, download status classifications from natureserve
-lep_stats <-ns_search_spp(species_taxonomy = list(
-  scientificTaxonomy = "Lepidoptera", level = "order",
-    kingdom = "Animalia")
-                             , location = list(nation ="US", subnation ="MD") #this filters to only include species that have a MD status, but retains status for all localities
-                             , page = 0
-                             , per_page = 5e3)[[1]] %>%
-  unnest(cols=nations) %>%
-  unnest (cols = "subnations", names_repair ="unique") %>%
-  filter(subnationCode == "MD") #here is the step where I drop other localities, but this could be dropped at some point.
-
-lep_stats %>% group_by(roundedSRank) %>% summarize(n())
-# of those 300ish sp, pretty evenly split between (S5, S4, [S1, {S2, S3}]), SNR. 
-
-#write just the natureserve data to file
-write.csv(lep_stats[,-21], "data/fromR/lfs/lep_NS_data.csv", row.names = F)
-
-lepstat<-read.csv("data/fromR/lfs/lep_NS_data.csv")
 
 # summary data
 lepstat %>% mutate(simple_status = factor(if_else(roundedSRank %in% c("S4","S5"), "secure"
--- code/download_occurrences_and_statuses.R
+++ code/download_occurrences_and_statuses.R
@@ -54,7 +54,7 @@ uscore_binomial<-function(x){gsub("+ + *", "+_+",x)}
 # MD_vasc_lep <- bind.gbif(GBIF_plant_lep)
 # after a bit of exploration I feel good about not going with this process.
 # instead unzip and just grab the occurrence data themselves for now
-unzip("data/fromR/lfs/0413674-210914110416597.zip", exdir = "data/fromR/lfs/GBIF_downloads")
+# unzip("data/fromR/lfs/0413674-210914110416597.zip", exdir = "data/fromR/lfs/GBIF_downloads")
 MD_plant_lep_occ <- data.table::fread("data/fromR/lfs/GBIF_downloads/occurrence.txt")
 # # note this returns a warning about parsing th efile (is it weird it gets demonic after line 666?)
 head(MD_plant_lep_occ)
@@ -100,36 +100,32 @@ head(MD_occ)
 #   mutate(gs = paste(genus, species, sep = "_")
 #          , withspace = paste(genus, species, sep = " ")) #convenient to keep track of binomials in several forms?
 
-plants_gs_new<-MD_occ %>%
-  filter(kingdomKey == 6) %>% 
+occ_gs <- MD_occ %>%
   separate(acceptedScientificName, sep =" " # this is the accepted name (i.e. most taxonomic issues resolved)
            , into =c("genus", "species")) %>% # separate just drops stuff after the first two!
   mutate(gs = paste(genus, species, sep = "_")
          , withspace = paste(genus, species, sep = " ")) #convenient to keep track of binomials in several forms?
 
-unique(plants_gs_new$gs)
+unique(occ_gs$gs)
 
-unique(plants_gs$species)
+unique(occ_gs$species)
 
-plants_gs %>% filter(species =="X")
+# obsolete cleanup
 
-plants_gs_new<-plants_gs_new %>% filter(!grepl("\\.", .$species))
+# occ_gs %>% filter(species =="X")
+# 
+# occ_g<-occ_gs %>% filter(!grepl("\\.", .$species))
 
 
 
 
 # write gbif data to file (make these steps modular since they take a long time)
-write.csv(plants_gs, "data/fromR/lfs/plants_direct_from_gbif.csv", row.names = F)
+write.csv(occ_gs, "data/fromR/lfs/occ_from_gbif.csv", row.names = F)
 
-plants_gs <- read.csv("data/fromR/lfs/plants_direct_from_gbif.csv")
-plants_gs_new
-plants_gs_new %>% filter(gs %in% plants_gs$gs) %>% summarize(n())
-# wow. Ok. 60k more records
-plants_gs_new %>% filter(gs %ni% plants_gs$gs) %>% summarize(spp = n_distinct(gs), occ = n())
-plants_gs_new %>% filter(gs %ni% plants_gs$gs) %>% pull(gs)
-head(plants_gs)
+occ_gs <- read.csv("data/fromR/lfs/occ_from_gbif.csv")
+
 #get a list of all binomials in the GBIF dataset
-plants_of_MD<-unique(plants_gs_new$withspace) #2620 1999-2021
+plants_of_MD<-unique(occ_gs %>%  filter(kingdomKey == 6) %>% pull(withspace)) #2695  with new data. 
 
 ###################################################
 
@@ -142,30 +138,59 @@ plant_stats <- ns_search_spp(species_taxonomy = list(scientificTaxonomy = "Plant
   unnest (cols = "subnations", names_repair ="unique") %>%
   filter(subnationCode == "MD") #here is the step where I drop other localities, but this could be dropped at some point.
 
+#merge status and occcurence one kingdom at a time. 
+lep_stats <-ns_search_spp(species_taxonomy = list(
+  scientificTaxonomy = "Lepidoptera", level = "order",
+  kingdom = "Animalia")
+  , location = list(nation ="US", subnation ="MD") #this filters to only include species that have a MD status, but retains status for all localities
+  , page = 0
+  , per_page = 5e3)[[1]] %>%
+  unnest(cols=nations) %>%
+  unnest (cols = "subnations", names_repair ="unique") %>%
+  filter(subnationCode == "MD") #here is the step where I drop other localities, but this could be dropped at some point.
+
 # drafted to deal with bad species names... will see if needed in the post-gis dataset
 # ps1<-plant_stats %>% separate(scientificName, sep =" "
 #          , into =c("genus", "species")) %>% # separate just drops stuff after the first two!
 #   mutate(gs = paste(genus, species, sep = "_")
 #          , withspace = paste(genus, species, sep = " "))
 # ps2 <- ps1 %>% filter(species %ni% c("x", "var", " ", "")
-#write just the natureserve data to file
-write.csv(plant_stats[,-21], "data/fromR/lfs/plant_NS_data.csv", row.names = F)
 
-plant_stats<-read.csv("data/fromR/lfs/plant_NS_data.csv") #generates an error, weird.
+##############################
+##### write and read NS data 
+
+# write just the natureserve data to file
+write.csv(plant_stats[,-21], "data/fromR/lfs/plant_NS_data.csv", row.names = F)
+write.csv(lep_stats[,-21], "data/fromR/lfs/lep_NS_data.csv", row.names = F)
+
+# read in archived data
+lepstat<-read.csv("data/fromR/lfs/lep_NS_data.csv")
+plant_stats<-read.csv("data/fromR/lfs/plant_NS_data.csv") 
+occ_gs <- read.csv("data/fromR/lfs/occ_from_gbif.csv")
+
+
+
+
+
+lep_joined <- occ_gs %>% 
+  filter(kingdomKey == 1)
+  left_join(lepstat, by=c("withspace"="scientificName")) %>% 
+  dplyr::filter((!exotic...15 & !exotic...17) |(is.na(exotic...15) & is.na(exotic...17))) %>% 
+  mutate(simple_status =ifelse(roundedSRank %in% c("S1", "S2", "S3", "SH"), "threat"
+                               , ifelse(roundedSRank %in% c("S4", "S5"), "secure"
+                                        , "unranked"))) 
+
+withstats<-plants_gs_new %>%
+  select(-gs) %>%
+  left_join(plant_stats[,-21], by=c("withspace"="scientificName"))
+
+
 
 # summary data
 # plant_stats %>% mutate(simple_status = factor(if_else(roundedSRank %in% c("S4","S5"), "secure"
 #                                                          , if_else(roundedSRank %in% c("S1", "S2", "S3", "SH"), "threatened", "NONE")))) %>%
 #   group_by(simple_status) %>% summarize(n())
 
-
-plants_gs<-read.csv("data/fromR/lfs/plants_direct_from_gbif.csv")
-
-
-#merge status and occcurrence
-withstats<-plants_gs_new %>%
-  select(-gs) %>%
-  left_join(plant_stats[,-21], by=c("withspace"="scientificName"))
 
 # don't delete info, but here is where we can simplify status (also an e.g.)
 withstats2<-withstats %>%
