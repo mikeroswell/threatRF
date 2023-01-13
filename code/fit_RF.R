c0467a410700fae7c7a544008d4a73285c9f6e1c
mikeroswell <mike.roswell@gmail.com>
2023-01-13 08:39:17 -0500

    Guess this was refit, but going to do again anyways

--- code/fit_RF.R
+++ code/fit_RF.R
@@ -46,7 +46,7 @@ trees_leps <- map(c( "plant", "lep"), function(tax){
 })
 
 
-save(trees_leps, file="data/fromR/lfs/100_100_fits_20230103.rda")
+save(trees_leps, file="data/fromR/lfs/100_100_fits_20230105.rda")
 
 
 write.csv(classed, "data/fromR/training_data.csv", row.names = FALSE)
